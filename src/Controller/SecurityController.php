a97b42a461e1dcaa15d33745884dec934876669b
Clément <clement.vidal2025@campus-eni.fr>
2025-09-19 16:10:59 +0200

    First commit : Theme Bootstrap + SecurityController + HomeController + Twig

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Affiche le message d'erreur de login s'il y en a un
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Affiche le dernier "username" entré par l'utilisateur
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/membre', name: 'app_membre')]
+    public function membre(): Response
+    {
+        return $this->render('membre.html.twig');
+    }
+
+}
